3d10018d24042af95294676e72dc2ccab4f365f1
secondshovon <108076266+secondshovon@users.noreply.github.com>
2024-11-23 19:44:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// jQuery
+$(document).ready(function(){
+ $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+});
+
+
+//wow 
+
+   wow = new WOW(
+     {
+     boxClass:     'wow',      // default
+     animateClass: 'animated', // default
+     offset:       0,          // default
+     mobile:       true,       // default
+     live:         true        // default
+   }
+   )
+   wow.init();
